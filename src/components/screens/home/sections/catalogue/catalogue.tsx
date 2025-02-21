c0586ba78f649793033d049a05a783db53e6c282
dan48ua <danyloyakovenko07@gmail.com>
2025-02-21 16:43:52 +0000

    prettier

--- src/components/screens/home/sections/catalogue/catalogue.tsx
+++ src/components/screens/home/sections/catalogue/catalogue.tsx
@@ -5,11 +5,13 @@ import React from 'react'
 import Candle from './candle/candle'
 
 const Catalogue: React.FC<ICandleData> = ({ candles }) => {
-	console.log(candles.length)
 	return (
 		<div style={{ display: 'flex', flexWrap: 'wrap' }}>
-			
-			{candles.length ? candles.map(candle => <Candle key={candle.id} candle={candle}/>) : <div>Candles not found!</div>}
+			{candles.length ? (
+				candles.map(candle => <Candle key={candle.id} candle={candle} />)
+			) : (
+				<div>Candles not found!</div>
+			)}
 		</div>
 	)
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Home from '@/components/screens/home/Home'
 import { ICandleData } from '@/interfaces/candle.interface'
 import { getCandles } from '@/services/candle.service'
-import { GetServerSideProps , NextPage } from 'next'
+import { GetServerSideProps, NextPage } from 'next'
 
 const Index: NextPage<ICandleData> = ({ candles }) => {
 	return (
@@ -11,11 +11,10 @@ const Index: NextPage<ICandleData> = ({ candles }) => {
 	)
 }
 
-export const getServerSideProps: GetServerSideProps <ICandleData> = async () => {
+export const getServerSideProps: GetServerSideProps<ICandleData> = async () => {
 	const candles = await getCandles.getAll()
-
 	return {
-		props: {candles}
+		props: { candles },
 	}
 }
 
