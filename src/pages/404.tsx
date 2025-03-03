970b155fcdf619c61fe77839b7e9b8b98f46fe3d
dan48ua <danyloyakovenko07@gmail.com>
2025-03-03 23:50:50 +0000

    404

---
+++ src/pages/404.tsx
@@ -0,0 +1,14 @@
+import Image from 'next/image'
+
+export default function Custom404() {
+	return (
+		<Image
+			src='/404/DALL·E 2025-03-03 23.47.20 - A dark yet warm and moody wide-angle image of a mourning candle in a black glass jar, symbolizing sadness and loss. The candle casts a dim but warm go.webp'
+			quality={100}
+			fill
+			sizes='100%'
+			alt='background'
+			style={{ objectFit: 'cover', objectPosition: 'center' }}
+		/>
+	)
+}
