9e48e840691ca595c476e57fdb5c00a4b5fe9b12
danyloyakovenko <151740616+dan48ua@users.noreply.github.com>
2025-05-26 11:53:31 +0100

    Update footer.tsx

--- src/components/layout/footer/footer.tsx
+++ src/components/layout/footer/footer.tsx
@@ -18,7 +18,7 @@ const Footer: FC = () => {
 							</div>
 						</div>
 						<div className={style.column}>
-							<a href='/' className={style.link}>
+							<a href='/contactus' className={style.link}>
 								Contact us
 							</a>
 							<a href='https://www.instagram.com/zapaliui_candles/' className={style.link}>
