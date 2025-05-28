5bf1611b49b3b5519479746c0788cf070b860c67
dan48ua <danyloyakovenko07@gmail.com>
2025-05-28 16:54:09 +0100

    okak

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,8 +2,16 @@ import Image from 'next/image'
 
 export default function Custom404() {
 	return (
+		// <Image
+		// 	src='/404/DALL·E 2025-03-03 23.47.20 - A dark yet warm and moody wide-angle image of a mourning candle in a black glass jar, symbolizing sadness and loss. The candle casts a dim but warm go.webp'
+		// 	quality={100}
+		// 	fill
+		// 	sizes='100%'
+		// 	alt='background'
+		// 	style={{ objectFit: 'cover', objectPosition: 'center' }}
+		// />
 		<Image
-			src='/404/DALL·E 2025-03-03 23.47.20 - A dark yet warm and moody wide-angle image of a mourning candle in a black glass jar, symbolizing sadness and loss. The candle casts a dim but warm go.webp'
+			src='/404/okak.webp'
 			quality={100}
 			fill
 			sizes='100%'
